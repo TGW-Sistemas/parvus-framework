0e9e46a20c50f7d4d3efb9fc3b170f5ee3fbf51c
Vinícius Fuhr de Borba <tecnologia@tgwsistemas.com.br>
2022-12-05 11:36:06 -0300

    Ajuste Input::get

--- src/Input.php
+++ src/Input.php
@@ -6,7 +6,7 @@
 
 		public final static function get ($prName,$prValue = NULL)
 		{
-			return $_REQUEST[$prName] != NULL ? trim(str_replace(array('<script>','</script>'),",",str_replace("'","´",$_REQUEST[$prName]))) : $prValue;
+			return $_REQUEST[$prName] != NULL ? trim(str_replace(array('<script>','</script>'),array('',''),str_replace("'","´",$_REQUEST[$prName]))) : $prValue;
 		}
 
         /**
